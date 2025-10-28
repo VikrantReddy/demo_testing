c6007fd2fa685ed12acde6eba7cd1ffb8777c84c
unknown <vikrantreddy@hotmail.com>
2025-10-28 05:45:17 +0530

    Added controller logic, and tests

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,30 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const students = await getAllStudents(req.query);
+    res.status(200).json(students);
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const result = await addNewStudent(req.body);
+    res.status(201).json(result);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const result = await updateStudent(req.body);
+    res.status(200).json(result);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    return res.status(200).json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = { userId: req.params.id, status: req.body.status, reviewerId: req.user.id };
+    const result = await setStudentStatus(payload);
+    return res.status(200).json(result);
 });
 
 module.exports = {
